5b0374a8894dc3822f5f16d5c9d9ce119c5f6225
Jake Weiss <32331702+JakeLWeiss@users.noreply.github.com>
2017-12-05 21:08:52 -0500

    test commit and comments

--- content.js
+++ content.js
@@ -1,16 +1,21 @@
 // content.js
- 
+
 $(document).ready(function () {
     var i = 0;
+    //sets a div id for all expando buttons on page load
     $('.expando-button').each(function () {
         $(this).attr('id', i);
         i++;
     });
     
-   
-    for (k = 0; k < i; k++) {
+   //clicks the expando
+    $('expando-button').click();
+
+
+   /* for (k = 0; k < i; k++) {
         $('#' + k).click();
         $('.expando').attr('style', 'type: block');
                
     }
+    */
 });
